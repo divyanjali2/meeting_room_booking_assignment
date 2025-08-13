4c67b6d1d67a6df5157e51d694992ad063ac4a9a
divyanjali2 <navodyadivyanjali2@gmail.com>
2025-08-13 21:24:22 +0530

    routes added

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookingController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,6 +16,19 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('/', fn() => to_route('bookings.index'));
+
+Route::middleware('auth')->group(function () {
+    Route::get('/bookings', [BookingController::class,'index'])->name('bookings.index');
+
+    // AJAX JSON endpoints
+    Route::get('/api/bookings', [BookingController::class,'list']);
+    Route::get('/api/bookings/mine', [BookingController::class,'mine']);
+    Route::post('/api/bookings', [BookingController::class,'store']);
+    Route::put('/api/bookings/{booking}', [BookingController::class,'update']);
+    Route::delete('/api/bookings/{booking}', [BookingController::class,'destroy']);
 });
 
 require __DIR__.'/auth.php';
