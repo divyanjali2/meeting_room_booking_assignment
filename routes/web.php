54b6ae65cb0982443d435b1d94688cbd6a8862e8
divyanjali2 <navodyadivyanjali2@gmail.com>
2025-08-13 22:08:27 +0530

    login directed to dashboard

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,12 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookingController;
 
-Route::get('/', fn() => to_route('bookings.index'));
+Route::get('/', function () {
+    if (auth()->check()) {
+        return redirect()->route('dashboard');
+    }
+    return view('auth.login');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -15,17 +20,6 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-Route::get('/bookings', function () {
-    return view('bookings');
-})->middleware(['auth', 'verified'])->name('bookings');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
 
 Route::middleware('auth')->group(function () {
     Route::get('/bookings', [BookingController::class,'index'])->name('bookings.index');
